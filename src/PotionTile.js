789a4ccd0befc36b8978e50871e36f3f4e5d9c99
github username <github email address>
2020-02-21 11:17:54 -0500

    morning dq before lecture

--- src/PotionTile.js
+++ src/PotionTile.js
@@ -5,12 +5,17 @@ Write an abstracted component that can be used in place of InventoryTile AND Sho
 
 Consider what the tiles have in common and what they do not: 
 - For things that are the same, copy and pasting works just fine! 
-- For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents 
+- For things that are different, consider how you might make use of propsand JSX to have the same component behave differently when rendered by different parents 
 */
 
 const PotionTile = (props) => {
   return (
-    null
+    <div key={props.potion.id} className="inventory-card">
+      <div>
+        <img className="image" alt={props.potion.name} src={props.potion.image_url} />
+      </div>
+      {props.children}
+    </div>
   )
 }
 
